b471f3da0ebc5cea6b347867cca64e61593b4015
Craig Drummond <craig.p.drummond@gmail.com>
2020-04-18 10:46:45 +0100

    Use sticky so that navigation controls disspear if not used

--- app/src/main/java/com/craigd/lmsmaterial/app/MainActivity.java
+++ app/src/main/java/com/craigd/lmsmaterial/app/MainActivity.java
@@ -118,7 +118,8 @@ public class MainActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     @Override
