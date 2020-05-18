a71fe06790e2f0178f657af52e9b70fc994b1461
Craig Drummond <craig.p.drummond@gmail.com>
2020-05-18 10:45:37 +0100

    Correctly handle vsibility of navbar when statubar is hidden.

--- lms-material/src/main/java/com/craigd/lmsmaterial/app/MainActivity.java
+++ lms-material/src/main/java/com/craigd/lmsmaterial/app/MainActivity.java
@@ -451,12 +451,6 @@ public class MainActivity extends AppCompatActivity {
         if (statusbar==BAR_HIDDEN) {
             if (navbar==BAR_HIDDEN) {
                 getWindow().getDecorView().setSystemUiVisibility(
-                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                | View.SYSTEM_UI_FLAG_FULLSCREEN);
-            } else {
-                getWindow().getDecorView().setSystemUiVisibility(
                         View.SYSTEM_UI_FLAG_IMMERSIVE
                                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -464,6 +458,12 @@ public class MainActivity extends AppCompatActivity {
                                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            } else {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN);
             }
         } else if (navbar==BAR_HIDDEN) {
             getWindow().getDecorView().setSystemUiVisibility(
