c82eb79244a0b83c37257eae52b2ee99fc5f0a80
CDrummond <craig.p.drummond@gmail.com>
2025-08-26 16:17:57 +0100

    Only apply adjustResize work-around if full screen (see https://issuetracker.google.com/issues/36911528)

--- lms-material/src/main/java/com/craigd/lmsmaterial/app/MainActivity.java
+++ lms-material/src/main/java/com/craigd/lmsmaterial/app/MainActivity.java
@@ -1282,6 +1282,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void possiblyResizeChildOfContent() {
+        if (!isFullScreen) {
+            return;
+        }
         int usableHeightNow = computeUsableHeight();
         if (usableHeightNow != usableHeightPrevious) {
             int usableHeightSansKeyboard = childOfContent.getRootView().getHeight();
